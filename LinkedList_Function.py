a6bab92b955d4eba2d9eb801ee36a326d522601e
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-02-11 21:26:05 -0500

    Linked_List printing nodes instead of as a string.
    Instead of returning a string, it will return the node at a more efficient manner.

--- LinkedList_Function.py
+++ LinkedList_Function.py
@@ -1,13 +1,11 @@
-def remove_lists(self, value_to_remove):
-        current_node = self.get_head_node() 
-        next_node = current_node.get_link()
+def remove_nodes(self, value):
+        current_node = self.get_head_node()
 
+        while current_node:
+            next_node = current_node.get_link()
+            if current_node.get_value() == value:
+                current_node = None
+            else:
+                print(current_node.get_value())
             
-        
-        if current_node.get_value() != None:
-            if current_node.get_value() == value_to_remove:
-                updated_string_list = self.stringify_list().replace(str(current_node.get_value()), "")
-                print(updated_string_list.strip("\n"))
-        #print(type(self.stringify_list()))
-        else:
             current_node = next_node
