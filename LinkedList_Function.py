03f8a4844a92b05003ed9187d106347c151ed7d3
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-02-12 09:49:47 -0500

    Added Function
    Added a Linked List function where it adds a node after a specified value.

--- LinkedList_Function.py
+++ LinkedList_Function.py
@@ -1,4 +1,4 @@
-def remove_nodes(self, value):
+def remove_nodes(self, value):                                                 #Removes certain nodes that have the specified value all at once
         current_node = self.get_head_node()
 
         while current_node:
@@ -11,7 +11,7 @@ def remove_nodes(self, value):
             current_node = next_node
                 
                 
-def insert_node_back(self, new_value):
+def insert_node_end(self, new_value):                                           #Adds a new node at the end of the Linked List instead the start
         new_node = Node(new_value)
         current_node = self.get_head_node()
 
@@ -24,4 +24,20 @@ def insert_node_back(self, new_value):
                 break
             else:
                 current_node = next_node 
+                
+                
+def insert_node_after_value(self, new_value, value):                           #Adds a new node after a certain node
+        current_node = self.get_head_node()
+        new_node = Node(new_value)
+
+        while current_node:
+            next_node = current_node.get_link()
+            next_next_node = next_node.get_link()
+            if next_node.get_value() == value:
+                next_node.set_link(new_node)
+                new_node.set_link(next_next_node)
+                break
+                
+            else:
+                current_node = next_node
 
