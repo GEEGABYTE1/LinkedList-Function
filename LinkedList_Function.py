721d336512d833e0671cdcc7add9e8687d7178f3
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-02-12 09:34:23 -0500

    New Function added
    The second function adds the node after the tail node rather than adding it before the head_node.

--- LinkedList_Function.py
+++ LinkedList_Function.py
@@ -9,3 +9,19 @@ def remove_nodes(self, value):
                 print(current_node.get_value())
             
             current_node = next_node
+                
+                
+def insert_node_back(self, new_value):
+        new_node = Node(new_value)
+        current_node = self.get_head_node()
+
+        while current_node:
+            next_node = current_node.get_link()
+
+            if current_node.get_link() == None:
+                current_node.set_link(new_node)
+                new_node.set_link(None)
+                break
+            else:
+                current_node = next_node 
+
